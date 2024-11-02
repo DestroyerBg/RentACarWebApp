2991613e8c743b6276b0e24242e21a4ee46ae578
DestroyerBg <99879227+DestroyerBg@users.noreply.github.com>
2024-11-02 14:20:35 +0200

    Moved Login functionality into AccountController and introduced a new LoginViewModel

---
+++ RentACar.Web/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RentACar.Data.Models;
+using RentACar.Web.ViewModels.Account;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace RentACar.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly ILogger<LoginViewModel> logger;
+
+        public AccountController(SignInManager<ApplicationUser> _signInManager, 
+            ILogger<LoginViewModel> _logger)
+        {
+            signInManager = _signInManager;
+            logger = _logger;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            SignInResult result = 
+                await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: true);
+
+            if (result.Succeeded)
+            {
+                logger.LogInformation("User logged in.");
+                return RedirectToAction("Index", "Home");
+            }
+            if (result.IsLockedOut)
+            {
+                logger.LogWarning("User account locked out.");
+                return RedirectToAction("Lockout");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Lockout()
+        {
+            return View();
+        }
+    }
+}
