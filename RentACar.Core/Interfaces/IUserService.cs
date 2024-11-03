95239a1c2441668b9e2b2968d2ded3285358fa75
DestroyerBg <99879227+DestroyerBg@users.noreply.github.com>
2024-11-03 12:15:54 +0200

    Introduced generic interface IUserInterface

---
+++ RentACar.Core/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RentACar.Core.Interfaces
+{
+    public interface IUserService<TUser> where TUser : IdentityUser
+    {
+        Task<TUser> CreateNewUserInstance();
+        Task<bool> RegisterUserAsync<T>(T model);
+        Task<bool> LoginUserAsync<T>(T model);
+        Task<bool> LogoutUserAsync<T>();
+    }
+}
