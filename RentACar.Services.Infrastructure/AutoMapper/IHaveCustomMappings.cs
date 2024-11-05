3b93854c83d3009f3ccf4c9360e91e061453bfa8
DestroyerBg <99879227+DestroyerBg@users.noreply.github.com>
2024-11-05 14:06:12 +0200

    Added AutoMapper support
    AutoMapper is added on new RentACar.Services.Infrastructure layer

---
+++ RentACar.Services.Infrastructure/AutoMapper/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace RentACar.Services.Infrastructure.AutoMapper
+{
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IProfileExpression configuration);
+    }
+}
---
+++ RentACar.Services.Infrastructure/AutoMapper/IMapFrom.cs
@@ -0,0 +1,6 @@
+﻿namespace RentACar.Services.Infrastructure.AutoMapper
+{
+    public interface IMapFrom<T>
+    {
+    }
+}
