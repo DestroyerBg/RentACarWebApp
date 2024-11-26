32b6c77e28d71463c1174b4dd0c0bcb5c361ba29
DestroyerBg <99879227+DestroyerBg@users.noreply.github.com>
2024-11-26 13:51:20 +0200

    Added admin area and configured endpoints

--- RentACar.Web/Program.cs
+++ RentACar.Web/Program.cs
@@ -67,14 +67,19 @@ app.UseAuthorization();
 
 app.UseSession();
 
-app.MapControllerRoute(
-    name: "areas",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
 
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
 await app.RunAsync();
