13ecfb0826ef4a5cf873efbdec786a9e906b0762
DestroyerBg <99879227+DestroyerBg@users.noreply.github.com>
2024-11-29 23:47:16 +0200

    Added hidden inputs for modelbinding in add car page

---
+++ RentACar.Web/wwwroot/js/checkbox-values.js
@@ -0,0 +1,26 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    function handleCheckboxGroup(groupClass, hiddenInputId) {
+        const checkboxes = document.querySelectorAll(`.${groupClass}`);
+        const hiddenInput = document.getElementById(hiddenInputId);
+
+        checkboxes.forEach(checkbox => {
+            checkbox.addEventListener("change", () => {
+                if (checkbox.checked) {
+         
+                    hiddenInput.value = checkbox.value;
+
+                    checkboxes.forEach(otherCheckbox => {
+                        if (otherCheckbox !== checkbox) {
+                            otherCheckbox.checked = false;
+                        }
+                    });
+                } else {
+                    hiddenInput.value = "";
+                }
+            });
+        });
+    }
+
+    handleCheckboxGroup("category-checkbox", "selectedCategory");
+    handleCheckboxGroup("location-checkbox", "selectedLocation");
+});
