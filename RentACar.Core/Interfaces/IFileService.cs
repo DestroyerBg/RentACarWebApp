3023c083ba54e477a56eaa4fbf0970259ff2029b
DestroyerBg <99879227+DestroyerBg@users.noreply.github.com>
2024-11-30 23:49:42 +0200

    Introduced IFileService which will interact with files on the server

---
+++ RentACar.Core/Interfaces/IFileService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace RentACar.Core.Interfaces
+{
+    public interface IFileService
+    {
+        Task<bool> SavePhotoToServerAsync(IFormFile file, string photoName);
+    }
+}
