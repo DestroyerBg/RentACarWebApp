3f654dec7328a5fe36d15a3f7a694c33348a0e08
DestroyerBg <gabriel-kulev@abv.bg>
2025-04-25 19:43:52 +0300

    Fixed issue with JSON path in JsonReader class

--- RentACar.Data/Helpers/JsonReader.cs
+++ RentACar.Data/Helpers/JsonReader.cs
@@ -4,7 +4,17 @@
     {
         public static string ReadJson(string jsonFileName)
         {
-            string solutionRoot = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", ".."));
+            string solutionRoot = string.Empty;
+
+            try
+            { 
+                solutionRoot = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", ".."));
+            }
+            catch (Exception e)
+            {
+                solutionRoot = AppContext.BaseDirectory;
+            }
+
             string filePath = Path.Combine(solutionRoot, "RentACar.Data", "Seeder", "JSON", jsonFileName);
             string jsonContent = File.ReadAllText(filePath);
 
